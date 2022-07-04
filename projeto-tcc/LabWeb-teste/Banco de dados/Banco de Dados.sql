c81ca4f848494c8977be12c3796d3dd494c75510
Lord-Souza <104236211+Lord-Souza@users.noreply.github.com>
2022-07-03 21:20:43 -0300

    Create Banco de Dados.sql

---
+++ projeto-tcc/LabWeb-teste/Banco de dados/Banco de Dados.sql
@@ -0,0 +1,7 @@
+insert into pessoas 
+(nome, nascimento, email, telefone, sexo, diabetico, hipertenso, gestante)
+values
+('Everton', '1995-04-23', 'everton23@gmail.com', '988745632', 'M', 'S', 'N', 'N');
+
+
+select * from pessoas;
