d9bd2d62dc248f960ba316149bb1f2b870f570f0
Lord-Souza <104236211+Lord-Souza@users.noreply.github.com>
2022-07-03 21:20:47 -0300

    Delete Banco de Dados.sql

--- Banco de Dados.sql
+++
@@ -1,7 +0,0 @@
-insert into pessoas 
-(nome, nascimento, email, telefone, sexo, diabetico, hipertenso, gestante)
-values
-('Everton', '1995-04-23', 'everton23@gmail.com', '988745632', 'M', 'S', 'N', 'N');
-
-
-select * from pessoas;
