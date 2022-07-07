929564d0f72fd82fb8cb74d0f263c6b71b0cb094
iamlorranny <102685558+iamlorranny@users.noreply.github.com>
2022-07-07 20:53:02 -0300

    Update conexao.php

--- projeto-tcc/LabWeb-teste/conexao.php
+++ projeto-tcc/LabWeb-teste/conexao.php
@@ -1,11 +1,11 @@
 <?php
-	$serve = "localhost";
+	$serve = "127.0.0.1:3306";
 	$user = "root";
 	$pass = "";
 	$bd = "LabWeb"
 
 	if ($conn = mysqli_connect($serve, $user, $pass, $bd)){
-		//echo "Conectado";
+		echo "Conectado";
 	}else {
 
 		echo "Erro";
