95d6ef09e12a7623441461e1900f0cd452822a09
iamlorranny <102685558+iamlorranny@users.noreply.github.com>
2022-07-07 20:59:41 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,17 @@
+<?php
+	$serve = "127.0.0.1";
+	$user = "root";
+	$pass = "";
+	$bd = "LabWeb"
+
+	if ($conn = mysqli_connect($serve, $user, $pass, $bd)){
+		echo "Conectado";
+	}else {
+
+		echo "Erro";
+	}
+		
+
+			}
+		}
+?>
